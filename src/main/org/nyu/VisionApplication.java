03d47187b968e7c8e837404b30cac545f8eda0a3
Brandon <b.mazey@nyu.edu>
2019-10-08 13:06:43 -0400

    adding TODO

--- src/main/org/nyu/VisionApplication.java
+++ src/main/org/nyu/VisionApplication.java
@@ -3,5 +3,6 @@ package main.org.nyu;
 public class VisionApplication {
     public static void main(String[] args) {
         System.out.println("working setup!");
+        // TODO - load image as gray-level array
     }
 }
