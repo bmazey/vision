0e204f5f918e5ea3a5ca955287b24c429d3907b8
Brandon <b.mazey@nyu.edu>
2019-10-08 13:37:35 -0400

    working!

--- src/main/org/nyu/VisionApplication.java
+++ src/main/org/nyu/VisionApplication.java
@@ -1,8 +1,17 @@
 package main.org.nyu;
 
+import main.org.nyu.image.ImageLoader;
+
+import java.io.File;
+
 public class VisionApplication {
-    public static void main(String[] args) {
-        System.out.println("working setup!");
+    public static void main(String[] args) throws Exception {
         // TODO - load image as gray-level array
+        File f = new File("C:\\workspace\\vision\\resources\\grayscale_cat.jpg");
+        ImageLoader loader = new ImageLoader();
+
+        // load image and print
+        int[][] image = loader.load(f);
+        loader.printImageArray(image);
     }
 }
