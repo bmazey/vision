8ae89b5d652053f7b9f42603c0e654d4818827f2
Brandon <b.mazey@nyu.edu>
2019-11-01 13:59:36 -0400

    color palette issues - as least we know where the problem is!

--- src/main/java/org/nyu/VisionApplication.java
+++ src/main/java/org/nyu/VisionApplication.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class VisionApplication {
     public static void main(String[] args) throws Exception {
         // TODO - load image as gray-level array
-        File f = new File("C:\\workspace\\vision\\resources\\Houses-225.bmp");
+        File f = new File("C:\\workspace\\vision\\resources\\Zebra-crossing-1.bmp");
         ImageLoader loader = new ImageLoader();
 
         // load image and print
@@ -27,7 +27,8 @@ public class VisionApplication {
         // loader.printImageArray(smooth);
 
         ImageWriter writer = new ImageWriter();
-        writer.write(smooth, "house-gaussian");
+        writer.write(smooth, "zebra-gaussian");
+        writer.write(image, "zebra-test");
 
         // sobel's operator
         SobelOperator sobel = new SobelOperator();
@@ -39,20 +40,20 @@ public class VisionApplication {
         int[][] ynormalized = sobel.normalizeGradientImage(ygradient);
         int[][] normalized = sobel.normalizeGradientImage(gradient);
 
-        writer.write(xnormalized, "house-sobel-Gx");
-        writer.write(ynormalized, "house-sobel-Gy");
-        writer.write(normalized, "house-sobel");
+        writer.write(xnormalized, "zebra-sobel-Gx");
+        writer.write(ynormalized, "zebra-sobel-Gy");
+        writer.write(normalized, "zebra-sobel");
 
         // non-maxima suppression
         NonMaximaSuppressor suppressor = new NonMaximaSuppressor();
         double[][] theta = suppressor.computeGradientAngle(xgradient, ygradient);
         int[][] suppressed = suppressor.suppress(gradient, theta);
-        writer.write(suppressed, "house-maxima");
+        writer.write(suppressed, "zebra-maxima");
 
         // double threshold
         DoubleThresholder thresholder = new DoubleThresholder();
         int[][] canny = thresholder.threshold(suppressed, gradient, theta, 60, 30);
-        writer.write(canny, "house-canny");
+        writer.write(canny, "zebra-canny");
 
     }
 }
--- src/main/java/org/nyu/image/ImageLoader.java
+++ src/main/java/org/nyu/image/ImageLoader.java
@@ -1,7 +1,11 @@
 package main.java.org.nyu.image;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
@@ -25,8 +29,11 @@ public class ImageLoader {
         // iterate over new array and populate grayscale values
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
+
+                // FIXME - a way to assert grayscale?
                 // 0 is selected as third argument because image is assumed to be grayscale
-                imageArray[i][j] = raster.getSample(i, j, 0);
+                // imageArray[i][j] = raster.getSample(i, j, 0);
+                imageArray[i][j] = image.getRGB(i, j) & 0xFF;
             }
         }
 
